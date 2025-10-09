1a353c23902dc4f1ccc25dd727225bb392ffe57b
eusebio suarez <eusebiosuaresmartines@gmail.com>
2025-10-09 06:26:24 -0500

    configuracion inicial

---
+++ src/main/java/com/global/GobalRent/GobalRentApplication.java
@@ -0,0 +1,13 @@
+package com.global.GobalRent;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GobalRentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GobalRentApplication.class, args);
+	}
+
+}
