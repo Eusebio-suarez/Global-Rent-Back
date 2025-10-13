72e7315fbdb067e0b83c96348a1c19a483b2c6a1
eusebio suarez <eusebiosuaresmartines@gmail.com>
2025-10-12 22:08:59 -0500

    proteger rutas regun el rol

--- src/main/java/com/global/GobalRent/security/config/SecurityConfig.java
+++ src/main/java/com/global/GobalRent/security/config/SecurityConfig.java
@@ -3,6 +3,7 @@ package com.global.GobalRent.security.config;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
@@ -13,6 +14,7 @@ import org.springframework.security.web.authentication.UsernamePasswordAuthentic
 import com.global.GobalRent.security.filters.JwtAuthorizationFilter;
 
 @Configuration
+@EnableMethodSecurity(prePostEnabled = true)
 public class SecurityConfig {
 
     @Autowired
