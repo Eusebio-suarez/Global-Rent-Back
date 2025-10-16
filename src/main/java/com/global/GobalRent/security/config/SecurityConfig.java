20ce3d7e08f5f2671b08e5d14656799f96790b2e
Eusebio-suarez <eusebiosuaresmartines@gmail.com>
2025-10-16 09:55:04 -0500

    configuración de cors

--- src/main/java/com/global/GobalRent/security/config/SecurityConfig.java
+++ src/main/java/com/global/GobalRent/security/config/SecurityConfig.java
@@ -9,6 +9,8 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
 import com.global.GobalRent.security.filters.JwtAuthorizationFilter;
 
@@ -36,5 +38,23 @@ public class SecurityConfig {
     @Bean
     public PasswordEncoder passwordEncoder(){
         return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public UrlBasedCorsConfigurationSource corsConfigurationSource(){
+
+        CorsConfiguration corsConfiguration = new CorsConfiguration();
+
+        corsConfiguration.addAllowedOrigin("http://localhost:4200");
+
+        corsConfiguration.addAllowedMethod("*");
+
+        corsConfiguration.addAllowedHeader("*");
+
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+
+        source.registerCorsConfiguration("/**", corsConfiguration);
+
+        return source;
     }
 }
